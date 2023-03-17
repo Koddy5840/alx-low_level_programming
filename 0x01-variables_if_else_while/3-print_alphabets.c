8734c96a17190a7f35f78f24a43cb8723bf58dec
Koddy5840 <KoddySmith12345@gmail.com>
2023-03-17 11:02:44 -0700

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,9 +9,11 @@
 int main(void)
 {
 	int ch;
-	for(ch = 'A'; ch <= 'Z'; ch++)
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
 	putchar(ch);
-	for(ch = 'a'; ch <= 'z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	putchar(ch);
+	putchar('\n');
 	return (0);
 }
