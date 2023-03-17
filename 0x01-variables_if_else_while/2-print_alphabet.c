793e798fba421e27f9ffc28dced572cdbf2a3553
Koddy5840 <KoddySmith12345@gmail.com>
2023-03-17 10:34:57 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -This is the main function
+ * Description: Prints the lower case alphabet
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	printf("abcdefghijklmnopqrstuvwxyz\n");
+	return (0);
+}
