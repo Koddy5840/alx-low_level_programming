5bffb6248363480e35916dbf96dc01ba041852a3
Koddy5840 <KoddySmith12345@gmail.com>
2023-03-18 08:11:04 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main-This is the main function
+* Description: Prints all single digit numbers of base 10
+* Return: Always 0 (Success)
+*
+*/
+int main(void)
+{
+	printf("0123456789\n");
+}
+
