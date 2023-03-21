4ca55ba4230b64343b2cf80a2b89616f2ba99ace
Koddy5840 <KoddySmith12345@gmail.com>
2023-03-21 08:06:32 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,14 @@
 #include "main.h"
 
-
+/**
+ * print_alphabet - main funtion
+ * Return: Always 0 (success)
+ */
 void print_alphabet(void)
-{ 
+{
 	int i;
-	
-	for( i= 'a'; i <= 'z'; i++)
+
+	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i + 0);
 	}
