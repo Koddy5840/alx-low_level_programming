598abead37a033873907ad85e3061d93c7b15af6
Koddy5840 <KoddySmith12345@gmail.com>
2023-03-21 05:37:01 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - this is the main function
+ *Description: prints _putchar
+ *Return: always 0 (Success)
+ *
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
