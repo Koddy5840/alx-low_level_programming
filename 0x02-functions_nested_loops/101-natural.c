e0d363286fdb19d8d18b64903b854173ce1dd8b2
Koddy5840 <KoddySmith12345@gmail.com>
2023-03-22 13:09:27 -0700

    Taskk 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - this is the main function
+ *Description: prints multiples of 5 and 3 and sum them
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 1; i < 10; i++)
+	{
+	if (i % 3 == 0 || i % 5 == 0)
+	{
+	sum = sum + i;
+	}
+	}
+	printf("%d", sum);
+	printf("\n");
+	return (0);
+}
--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,45 +9,34 @@ void print_to_98(int n)
 {
 	int a;
 
-	if (n >= 0 && n <= 98)
-	{
-		for(a = n; a <= 98; a++)
-		{
-			printf("%d",a);
-			 if( n < 98)
-			 { 
+	if (n >= 0 && n <= 98){
+		for (a = n; a <= 98; a++){
+			printf("%d", a);
+			if (a != 98){
 				 printf(", ");
-			 }
+			}
 		}
 		printf("\n");
 	}
-	 else if(n > 98)
-		{
-			for(a = n; a >= 98; a--)
-			{
-			printf("%d",a);
-			if( n < 98)
-			{
+	else if (n > 98){
+			for (a = n; a >= 98; a--){
+			printf("%d", a);
+			if (a != 98){
 				printf(", ");
 			}
 			}
 		printf("\n");
 		}
-	else if(n < 0)
-	{
-		for(a = n; a <= 98; a++)
-		{
-			printf("%d",a);
-			if (n < 98)
-			{
+	else if (n < 0){
+		for (a = n; a <= 98; a++){
+			printf("%d", a);
+			if (a < 98){
 				printf(", ");
 			}
-
 		}
 		printf("\n");
 	}
-	else if (n == 98)
-	{
-		printf("%d",n);
+	else if (n == 98){
+		printf("%d", n);
 	}
 }
