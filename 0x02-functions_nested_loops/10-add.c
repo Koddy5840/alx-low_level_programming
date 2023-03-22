1598bb4bca683a983a3b697d5073ad63593b9129
Koddy5840 <KoddySmith12345@gmail.com>
2023-03-22 10:48:20 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *add -  this is the main function
+ *@a: parameter a
+ *@b: parameter b
+ *Return: Always add
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
