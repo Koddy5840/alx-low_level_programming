3858ab1058b04f419e13287fef13b418932f02ea
Koddy5840 <KoddySmith12345@gmail.com>
2023-04-08 06:02:32 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main - this is main function
+ *@argv: first argument of the function
+ *@argc: second argument of the function
+ *Return 0: void
+ */
+int main(int argc,char *argv[])
+{
+	if (argc == 0 || argc > 0)
+	{
+	printf("%d\n", argc - 1);
+	}
+	else
+	{
+	printf("%s", *argv);
+	}
+	return (0);
+}
